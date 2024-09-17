9f4bcfcd9fb82acf8807de34530f4d5e84bb0a43
Raigester <vovaraigester@gmail.com>
2024-09-17 21:38:48 +0300

    rename route in urls

--- vsizabihi/urls.py
+++ vsizabihi/urls.py
@@ -24,5 +24,5 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('event/', include('event.urls')),
-    path('filters/', include('event_filters.urls')),
+    path('calendar/', include('event_filters.urls')),
 ]
