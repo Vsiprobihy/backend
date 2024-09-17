f3341666d066c6b1bf0f2f908d2c35bba6e45658
Raigester <vovaraigester@gmail.com>
2024-09-17 21:42:43 +0300

    rename route in urls event_fillter

--- event_filters/urls.py
+++ event_filters/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import EventFilterView
 
 urlpatterns = [
-    path('events/filter/', EventFilterView.as_view(), name='event-filter'),
+    path('filter/', EventFilterView.as_view(), name='event-filter'),
 ]
