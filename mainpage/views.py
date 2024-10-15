9509dc4b585b75d5c8502b9007fe39be916ff293
Raigester <vovaraigester@gmail.com>
2024-10-15 20:10:30 +0300

    refactor main-page and rename url route to upcoming-events

--- mainpage/views.py
+++ mainpage/views.py
@@ -1,31 +1,36 @@
 from django.utils import timezone
-from django.conf import settings
 from event.models import Event
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 from rest_framework import status
 
 @swagger_auto_schema(
     method='get',
-    responses={200: 'Successful Response', 400: 'Invalid parameters'}
+    responses={200: 'Successful Response', 400: 'Invalid parameters'},
+    manual_parameters=[
+        openapi.Parameter(
+            name='count',
+            in_=openapi.IN_QUERY,
+            type=openapi.TYPE_INTEGER,
+            required=False,
+            description='Number of upcoming events to return (default is 3)',
+        )
+    ]
 )
 @api_view(['GET'])
 def mainpage(request):
     """
-    API endpoint for receiving events that will happen in the next 5 days.
+    API endpoint for receiving the next upcoming events.
     """
     today = timezone.now().date()
     
-    # Get start and end days from settings.py for request to database
-    start_date_offset = int(request.GET.get('start_date', settings.MAIN_PAGE_START_DATE_DAYS_AHEAD))
-    end_date_offset = int(request.GET.get('end_date', settings.MAIN_PAGE_EVENT_DAYS_AHEAD))
+    # Get the count of events to return from the request or default to 3
+    count = int(request.GET.get('count', 3))
 
-    # Calculate dates to filter
-    start_date = today + timezone.timedelta(days=start_date_offset)
-    end_date = today + timezone.timedelta(days=end_date_offset)
-
-    events = Event.objects.filter(date_from__range=(start_date, end_date))
+    # Filter upcoming events
+    upcoming_events = Event.objects.filter(date_from__gte=today).order_by('date_from')[:count]
 
     event_data = [
         {
@@ -37,7 +42,7 @@ def mainpage(request):
             'competition_type': event.competition_type,
             'photos': event.photos.url if event.photos else None,
         }
-        for event in events
+        for event in upcoming_events
     ]
 
     return Response({'events': event_data}, status=status.HTTP_200_OK)
