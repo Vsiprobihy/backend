f26ba5b18a1ff9d6057a2ede4b5ab956d68449e9
Raigester <vovaraigester@gmail.com>
2024-10-21 11:06:51 +0300

    fix connect django to postgreSQL

--- vsizabihi/settings.py
+++ vsizabihi/settings.py
@@ -79,10 +79,15 @@ WSGI_APPLICATION = 'vsizabihi.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'probihy_db',
+        'USER': 'vsi_probihy_user',
+        'PASSWORD': 'probihy_password',
+        'HOST': 'data-base',
+        'PORT': '5432',
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
