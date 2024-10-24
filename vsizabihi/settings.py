a9fe72888d78372420932c96df54120fb5ee5752
Raigester <vovaraigester@gmail.com>
2024-10-24 16:41:58 +0300

    add auth to swagger by JWT

--- vsizabihi/settings.py
+++ vsizabihi/settings.py
@@ -135,6 +135,9 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
+    ),
 }
 
 AUTHENTICATION_BACKENDS = (
@@ -157,7 +160,7 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
@@ -165,3 +168,15 @@ SIMPLE_JWT = {
 
 MAIN_PAGE_START_DATE_DAYS_AHEAD = 1  # Количество дней для начальной даты (для ендпоинта upcoming-events)
 MAIN_PAGE_EVENT_DAYS_AHEAD = 5  # Количество дней, до которого отображаем события (для ендпоинта upcoming-events)
+
+SWAGGER_SETTINGS = {
+    'USE_SESSION_AUTH': False,
+    'SECURITY_DEFINITIONS': {
+        'Bearer': {
+            'type': 'apiKey',
+            'name': 'Authorization',
+            'in': 'header',
+            'description': 'JWT Authorization header using the Bearer scheme. Example: "Bearer {your JWT token}"'
+        }
+    },
+}
