598583752ba3cfbce71541dac5199d55f50cbbed
Raigester <vovaraigester@gmail.com>
2024-10-24 22:38:06 +0300

    fix rest framework delete isAuthentificated

--- vsizabihi/settings.py
+++ vsizabihi/settings.py
@@ -135,9 +135,6 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.IsAuthenticated',
-    ),
 }
 
 AUTHENTICATION_BACKENDS = (
