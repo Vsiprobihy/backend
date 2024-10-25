811a64c068739c3b47d81949c370fea07da695f2
Raigester <vovaraigester@gmail.com>
2024-10-26 01:28:09 +0300

    delete unused import

--- authentication/views.py
+++ authentication/views.py
@@ -6,7 +6,6 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
-from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework_simplejwt.tokens import RefreshToken
 from drf_yasg import openapi
 
