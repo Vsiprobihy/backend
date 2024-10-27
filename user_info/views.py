0093b99c39d3b817f7d52d3bfc377cc4248e94bf
Raigester <vovaraigester@gmail.com>
2024-10-28 00:49:59 +0200

    add take avatar in /user/user-info/

--- user_info/views.py
+++ user_info/views.py
@@ -10,7 +10,9 @@ class UserInfoView(APIView):
     def get(self, request):
         if request.user.is_authenticated:
             user_name = f"{request.user.first_name} {request.user.last_name}"
+            avatar_url = request.build_absolute_uri(request.user.avatar.url) if request.user.avatar else None
         else:
             user_name = "User"
+            avatar_url = None
 
-        return Response({"username": user_name})
+        return Response({"username": user_name, "avatar": avatar_url})
