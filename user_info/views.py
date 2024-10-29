6c2202c6b394877d4cbe8081f2a3d976fe1dfeb9
Raigester <vovaraigester@gmail.com>
2024-10-29 18:31:38 +0200

    refactor user info page. Add return null if last and first name is none

--- user_info/views.py
+++ user_info/views.py
@@ -1,18 +1,22 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import IsAuthenticated
 from .swagger_schemas import user_info_response
 
 class UserInfoView(APIView):
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
 
     @user_info_response
     def get(self, request):
         if request.user.is_authenticated:
-            user_name = f"{request.user.first_name} {request.user.last_name}"
+            if request.user.first_name or request.user.last_name:
+                user_name = f"{request.user.first_name} {request.user.last_name}".strip()
+            else:
+                user_name = None
+            
             avatar_url = request.build_absolute_uri(request.user.avatar.url) if request.user.avatar else None
         else:
-            user_name = "User"
+            user_name = None
             avatar_url = None
 
         return Response({"username": user_name, "avatar": avatar_url})
