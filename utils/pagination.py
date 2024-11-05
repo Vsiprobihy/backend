f071c2cddef08c4f78fa96d312c2954c6edd6e53
Raigester <vovaraigester@gmail.com>
2024-11-05 23:30:26 +0200

    fix competition_type filter

--- utils/pagination.py
+++ utils/pagination.py
@@ -2,6 +2,7 @@ from django.core.paginator import Paginator
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+
 from event.serializers.events import EventSerializer
 
 class EventPaginationView(APIView):
