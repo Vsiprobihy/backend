da212ad34276fc086b0508cc4ad8a1d5be48a221
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-11-17 11:35:02 +0200

    comme back slash

--- organizer_event/urls.py
+++ organizer_event/urls.py
@@ -11,10 +11,10 @@ from organizer_event.views import (
 
 
 urlpatterns = [
-    path('events', EventsListView.as_view(), name='events_list'),
-    path('events/create', EventCreateView.as_view(), name='event_create'),
-    path('events/<int:id>', EventDetailView.as_view(), name='event_detail'),
-    path('events/<int:id>/update', EventUpdateView.as_view(), name='event_update'),
-    path('events/<int:id>/partial-update', EventPartialUpdateView.as_view(), name='event_partial_update'),
-    path('events/<int:id>/delete', EventDeleteView.as_view(), name='event_delete'),
+    path('events/', EventsListView.as_view(), name='events_list'),
+    path('events/create/', EventCreateView.as_view(), name='event_create'),
+    path('events/<int:id>/', EventDetailView.as_view(), name='event_detail'),
+    path('events/<int:id>/update/', EventUpdateView.as_view(), name='event_update'),
+    path('events/<int:id>/partial-update/', EventPartialUpdateView.as_view(), name='event_partial_update'),
+    path('events/<int:id>/delete/', EventDeleteView.as_view(), name='event_delete'),
 ]
