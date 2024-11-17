cf852dfe9b6da0aded638404af182e0e740a1359
Raigester <vovaraigester@gmail.com>
2024-11-18 01:53:43 +0200

    add request body to swagger for event update methods

--- swagger/event.py
+++ swagger/event.py
@@ -40,66 +40,7 @@ class SwaggerDocs:
     class EventUpdate:
         put = {
             'tags': ['Events'],
-            'request_body': openapi.Schema(
-                type=openapi.TYPE_OBJECT,
-                properties={
-                    'name': openapi.Schema(
-                        type=openapi.TYPE_STRING, description='Name of the event'
-                    ),
-                    'competition_type': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Type of competition',
-                        default='running',
-                    ),
-                    'date_from': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_DATE,
-                        description='Event start date',
-                    ),
-                    'date_to': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_DATE,
-                        description='Event end date',
-                    ),
-                    'place': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Location of the event',
-                        default='Lviv',
-                    ),
-                    'place_region': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Location of the event',
-                        default='lviv_region',
-                    ),
-                    'description': openapi.Schema(
-                        type=openapi.TYPE_STRING, description='Event description'
-                    ),
-                    'registration_link': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_URI,
-                        description='Registration link',
-                    ),
-                    'hide_participants': openapi.Schema(
-                        type=openapi.TYPE_BOOLEAN,
-                        description='Whether to hide participants',
-                    ),
-                    'extended_description': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Extended description of the event',
-                    ),
-                },
-                required=[
-                    'name',
-                    'competition_type',
-                    'date_from',
-                    'date_to',
-                    'place',
-                    'place_region',
-                    'description',
-                    'registration_link',
-                    'hide_participants',
-                ],
-            ),
+            'request_body': Request.EventRequestBody,
             'responses': {
                 200: Responce.EventResponse,
                 404: 'Event not found',
@@ -110,55 +51,7 @@ class SwaggerDocs:
     class EventPartialUpdate:
         patch = {
             'tags': ['Events'],
-            'request_body': openapi.Schema(
-                type=openapi.TYPE_OBJECT,
-                properties={
-                    'name': openapi.Schema(
-                        type=openapi.TYPE_STRING, description='Name of the event'
-                    ),
-                    'competition_type': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Type of competition',
-                        default='running',
-                    ),
-                    'date_from': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_DATE,
-                        description='Event start date',
-                    ),
-                    'date_to': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_DATE,
-                        description='Event end date',
-                    ),
-                    'place': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Location of the event',
-                        default='Lviv',
-                    ),
-                    'place_region': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Location of the event',
-                        default='lviv_region',
-                    ),
-                    'description': openapi.Schema(
-                        type=openapi.TYPE_STRING, description='Event description'
-                    ),
-                    'registration_link': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        format=openapi.FORMAT_URI,
-                        description='Registration link',
-                    ),
-                    'hide_participants': openapi.Schema(
-                        type=openapi.TYPE_BOOLEAN,
-                        description='Whether to hide participants',
-                    ),
-                    'extended_description': openapi.Schema(
-                        type=openapi.TYPE_STRING,
-                        description='Extended description of the event',
-                    ),
-                },
-            ),
+            'request_body': Request.EventRequestBody,
             'responses': {
                 200: Responce.EventResponse,
                 404: 'Event not found',
