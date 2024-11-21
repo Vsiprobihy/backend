8df74d25d5ee538dd317caac76790b7828adf0a8
jstrieb/github-stats <vadnetvadnet@ukr.net>
2024-11-21 13:53:04 +0200

    Update decorators.py

--- organization/decorators.py
+++ organization/decorators.py
@@ -10,7 +10,13 @@ from utils.custom_exceptions import BadRequestError, NotFoundError
 
 
 def check_organization_access_decorator(event_extractor):
+    """
+    A decorator to check if the user has access to the organization associated with the event.
 
+    :param event_extractor: A function that extracts the Event instance from the request and parameters.
+    :raises PermissionDenied: If the user does not have access to the organization.
+    :raises Http404: If the extracted Event does not exist.
+    """
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(self, request, *args, **kwargs):
@@ -38,27 +44,62 @@ def check_organization_access_decorator(event_extractor):
 
 
 def extract_event_from_distance(request, *args, **kwargs):
+    """
+    Extracts the Event instance associated with the given DistanceEvent.
+
+    :param request: The HTTP request object.
+    :raises DistanceEvent.DoesNotExist: If the DistanceEvent with the given ID does not exist.
+    :return: The associated Event instance.
+    """
     distance_id = kwargs.get('distance_id')
     distance = DistanceEvent.objects.select_related('event').get(pk=distance_id)
     return distance.event
 
 
 def extract_event_directly(request, *args, **kwargs):
+    """
+    Extracts the Event instance directly using the event_id parameter.
+
+    :param request: The HTTP request object.
+    :raises Event.DoesNotExist: If the Event with the given ID does not exist.
+    :return: The Event instance.
+    """
     event_id = kwargs.get('event_id')
     return Event.objects.get(pk=event_id)
 
 
 def extract_for_event_access_directly(request, *args, **kwargs):
+    """
+    Extracts the Event instance directly using the event_id parameter.
+
+    :param request: The HTTP request object.
+    :raises Event.DoesNotExist: If the Event with the given ID does not exist.
+    :return: The Event instance.
+    """
     event_id = kwargs.get('event_id')
     return Event.objects.get(pk=event_id)
 
 
 def extract_organization_directly(request, *args, **kwargs):
+    """
+    Extracts the Organization instance directly using the organizer_id parameter.
+
+    :param request: The HTTP request object.
+    :raises OrganizationAccess.DoesNotExist: If the Organization with the given ID does not exist.
+    :return: The Organization instance.
+    """
     organizer_id = kwargs.get('organizer_id')
     return OrganizationAccess.objects.get(pk=organizer_id)
 
 
 def check_organizer_access_decorator(organization_extractor):
+    """
+    A decorator to check if the user has access to the organization associated with the event.
+
+    :param event_extractor: A function that extracts the Event instance from the request and parameters.
+    :raises PermissionDenied: If the user does not have access to the organization.
+    :raises Http404: If the extracted Event does not exist.
+    """
     def decorator(view_func):
         @wraps(view_func)
         def _wrapped_view(self, request, *args, **kwargs):
